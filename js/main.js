1d0f923b7894c8404ac458e08ff38aca7cfca662
nickiiiiy <137485316+nickiiiiy@users.noreply.github.com>
2023-10-11 15:50:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.bike-slider, .slider-items').slick({
+        fade:true,
+        arrows: false,
+        dots:true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
